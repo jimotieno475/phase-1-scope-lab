6046083585e0be5e50e6e18937871e10657a9373
jim <jim.otieno@student.moringaschool.com>
2023-09-22 22:42:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+
+window.customerName="bob";
+window.bestCustomer;
+
+function upperCaseCustomerName() {
+    window.customerName=window.customerName.toUpperCase();
+}
+function setBestCustomer() {
+     window.bestCustomer = 'not bob';
+      
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    
+        window.bestCustomer=newBestCustomer
+        
+}
+const leastFavoriteCustomer = "jim";
+
+function changeLeastFavoriteCustomer(newCustomer) {
+    leastFavoriteCustomer = newCustomer;
+    changeLeastFavoriteCustomer= "tom";
+}
